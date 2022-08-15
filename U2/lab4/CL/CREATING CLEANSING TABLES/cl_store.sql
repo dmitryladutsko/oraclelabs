7f3b9ef098bd47264e91268731b6a8bb92800d80
dmitryladutsko <dmitryladutsko1207@gmail.com>
2022-08-15 12:21:17 +0300

    minor fix 4 lab

--- U2/lab4/CL/CREATING CLEANSING TABLES/cl_store.sql
+++ U2/lab4/CL/CREATING CLEANSING TABLES/cl_store.sql
@@ -1,10 +1,10 @@
-alter session set current_schema = SA_DATE;
+alter session set current_schema = DW_CLEANSING;
 
-CREATE TABLE SA_STORES (
-    phone         VARCHAR2(40 BYTE) NOT NULL,
-    address       VARCHAR2(20 BYTE) NOT NULL,
-    city          VARCHAR2(20 BYTE) NOT NULL,
-    country       VARCHAR2(20 BYTE) NOT NULL,
-    region        VARCHAR2(20 BYTE) NOT NULL
+CREATE TABLE CL_STORES (
+    country_city  VARCHAR2(20),
+    address       VARCHAR2(20),
+    phone         NUMBER 
 );
 
+
+select * from cl_stores;
--- U2/lab4/DW/CREATING DIMENSION TABLES/dim_store.sql
+++ U2/lab4/DW/CREATING DIMENSION TABLES/dim_store.sql
@@ -1,12 +1,11 @@
 alter session set current_schema = DW_DATA;
 
-CREATE TABLE DIM_store (
-    store_id      NUMBER(10) NOT NULL,
-    manager_id    NUMBER(10) NOT NULL,
-    phone         VARCHAR2(40 BYTE) NOT NULL,
-    address       VARCHAR2(20 BYTE) NOT NULL,
-    city          VARCHAR2(20 BYTE) NOT NULL,
-    country       VARCHAR2(20 BYTE) NOT NULL,
-    region        VARCHAR2(20 BYTE) NOT NULL,
+SELECT * FROM  DIM_STORES;
+
+CREATE TABLE DIM_stores (
+    store_id      INT,
+    country_city  VARCHAR2(20) NOT NULL,
+    address       VARCHAR2(20) NOT NULL,
+    phone         NUMBER NOT NULL,
     CONSTRAINT store_id_pk PRIMARY KEY ( store_id )
 );
