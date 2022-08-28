714e4824cd3da102066cb74614090202bae1e764
dmitryladutsko <dmitryladutsko1207@gmail.com>
2022-08-28 17:15:37 +0300

    Laboratory Work 3 Repot + results + scripts + SSIS packages READY FULLY

--- U3/lab3/create database and tables.sql
+++ U3/lab3/create database and tables.sql
@@ -12,6 +12,34 @@ CREATE TABLE dbo.EmployeesExternal
  StateProvinceName NVARCHAR(50) NOT NULL, 
  CountryRegionName NVARCHAR(50) NOT NULL);
 
+ SELECT 
+    e.[BusinessEntityID] as [EmployeeID]
+    ,p.[FirstName]
+    ,p.[LastName]
+    ,e.[JobTitle]  
+    ,ea.[EmailAddress]
+    ,a.[City]
+    ,sp.[Name] AS [StateProvinceName] 
+    ,cr.[Name] AS [CountryRegionName] 
+FROM [HumanResources].[Employee] e
+  INNER JOIN [Person].[Person] p
+  ON p.[BusinessEntityID] = e.[BusinessEntityID]
+    INNER JOIN [Person].[BusinessEntityAddress] bea 
+    ON bea.[BusinessEntityID] = e.[BusinessEntityID] 
+    INNER JOIN [Person].[Address] a 
+    ON a.[AddressID] = bea.[AddressID]
+    INNER JOIN [Person].[StateProvince] sp 
+    ON sp.[StateProvinceID] = a.[StateProvinceID]
+    INNER JOIN [Person].[CountryRegion] cr 
+    ON cr.[CountryRegionCode] = sp.[CountryRegionCode]
+    LEFT OUTER JOIN [Person].[PersonPhone] pp
+    ON pp.BusinessEntityID = p.[BusinessEntityID]
+    LEFT OUTER JOIN [Person].[PhoneNumberType] pnt
+    ON pp.[PhoneNumberTypeID] = pnt.[PhoneNumberTypeID]
+    LEFT OUTER JOIN [Person].[EmailAddress] ea
+    ON p.[BusinessEntityID] = ea.[BusinessEntityID]
+		order by p.[BusinessEntityID] asc;
+
 --DROP TABLE dbo.EmployeesAW;
 
 CREATE TABLE dbo.EmployeesAW
@@ -20,6 +48,12 @@ CREATE TABLE dbo.EmployeesAW
  LastName NVARCHAR(50) NOT NULL,
  JobTitle NVARCHAR(50) NULL);
 
+ select p.[BusinessEntityID],[FirstName],[LastName],[JobTitle]
+	from [Person].[Person] p
+		inner join [HumanResources].[Employee] e
+			on p.BusinessEntityID = e.BusinessEntityID;
+		select * from dbo.EmployeesAW
+
 --DROP TABLE dbo.EmailAddressesAW;
 
 CREATE TABLE dbo.EmailAddressesAW
@@ -27,3 +61,18 @@ CREATE TABLE dbo.EmailAddressesAW
  BusinessEntityID INT NOT NULL,
  EmailAddress NVARCHAR(50) NOT NULL);
 
+ select [EmailAddressID],p.[BusinessEntityID],[EmailAddress]
+	from [Person].[EmailAddress] e
+		inner join [Person].[Person] p
+			on e.[BusinessEntityID] = p.BusinessEntityID
+			where p.[BusinessEntityID] < 291
+
+
+
+			truncate table dbo.EmployeesAW;
+			truncate table dbo.EmailAddressesAW; 
+			truncate table dbo.EmployeesExternal;
+
+			select * from dbo.EmployeesAW;
+			select * from dbo.EmailAddressesAW; 
+			select * from dbo.EmployeesExternal;
